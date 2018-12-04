73e5a6b4e68df31f3cda2f918406fb3543f0c66e
ngntrn <trannga@oregonstate.edu>
2018-12-04 11:34:58 -0800

    Update TeaTableViewCell.swift

--- TeaTableViewCell.swift
+++ TeaTableViewCell.swift
@@ -8,34 +8,20 @@
 import UIKit
 
 class TeaTableViewCell: UITableViewCell {
-
+    
     //MARK: Properties
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var secsLabel: UILabel!
-<<<<<<< HEAD
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-=======
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        //self.addShadow()
-        // Initialization code
->>>>>>> 5668bf26eed387a1cf351eaf1568ce74a5a61b98
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-<<<<<<< HEAD
-
-=======
         
-        // Configure the view for the selected state
->>>>>>> 5668bf26eed387a1cf351eaf1568ce74a5a61b98
     }
 }
 
